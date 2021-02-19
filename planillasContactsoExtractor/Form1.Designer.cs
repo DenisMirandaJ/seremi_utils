c648349a838419462eca655720899a89d59e8353
Denis <denis.mj@outlook.com>
2021-02-19 12:15:29 -0300

    ADD; extractor info planillas contactos estrechos

---
+++ planillasContactsoExtractor/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace planillasContactsoExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Enabled = false;
+            this.folderTextBox.Location = new System.Drawing.Point(44, 42);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(398, 20);
+            this.folderTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Carpeta selecionada";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(458, 42);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(111, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Selecionar carpeta";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(458, 94);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(111, 23);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Empezar";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 129);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.folderTextBox);
+            this.Name = "Form1";
+            this.Text = "Extractor de info planillas de contactos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ planillasContactsoExtractor/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace planillasContactsoExtractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private IDictionary<string, string> extractDataFromContactosFile(string filename)
+        {
+            IDictionary<string, string> row = new Dictionary<string, string>();
+            var fi = new FileInfo(filename);
+            using (var p = new ExcelPackage(fi))
+            {
+                var ws = p.Workbook.Worksheets[0];
+                if (!ws.Cells[12, 2].Value.ToString().Contains("Nombre Empresa"))
+                    return null;
+                if (!ws.Cells[12, 7].Value.ToString().Contains("Nombre caso positivo"))
+                    return null;
+                if (!ws.Cells[22, 2].Value.ToString().Contains("Nombre del trabajador"))
+                    return null;
+
+                //Datos empresa
+                row.Add("Nombre Empresa", ws.Cells[12, 3].Value.ToString());
+                row.Add("Rut Empresa", ws.Cells[13, 3].Value.ToString());
+                row.Add("Nombre OAL", ws.Cells[14, 3].Value.ToString());
+                row.Add("Región", ws.Cells[15, 3].Value.ToString());
+                row.Add("Comuna", ws.Cells[16, 3].Value.ToString());
+                row.Add("Persona responsable", ws.Cells[17, 3].Value.ToString());
+                row.Add("Telefono responsable", ws.Cells[18, 3].Value.ToString());
+                //DAtos caso
+                row.Add("Nombre caso positivo", ws.Cells[12, 8].Value.ToString());
+                row.Add("Rut caso positivo", ws.Cells[13, 8].Value.ToString());
+                row.Add("Teléfono caso positivo", ws.Cells[14, 8].Value.ToString());
+                row.Add("Folio Epivigila", ws.Cells[15, 8].Value.ToString());
+                //Numero contactos
+                int contactosCount = 0;
+                int firstContactoRowIndex = 23;
+                ExcelRange aux = ws.Cells[firstContactoRowIndex, 2];
+                while (aux.Value != null)
+                {
+                    contactosCount++;
+                    firstContactoRowIndex++;
+                    aux = ws.Cells[firstContactoRowIndex, 2];
+                }
+
+                row.Add("Numero contactos", contactosCount.ToString());
+            }
+            return row;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(folderTextBox.Text);
+            string[] extensions = new[] { ".xls", ".xlsx" };
+            FileInfo[] files = d.GetFiles().Where(f => extensions.Contains(f.Extension.ToLower())).ToArray();
+            DirectoryInfo noProcesadorDir = Directory.CreateDirectory(Path.Combine(folderTextBox.Text, "no procesados"));
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.Commercial;
+            var p = new ExcelPackage();
+            var ws = p.Workbook.Worksheets.Add("Hoja 1");
+            var headers = new string[] { "Nombre Empresa", "Rut Empresa", "Nombre OAL", "Región", "Comuna", "Persona responsable", "Telefono responsable", "Nombre caso positivo", "Rut caso positivo", "Teléfono caso positivo", "Folio Epivigila", "Numero contactos" };
+            int i = 1;
+            foreach (string header in headers)
+            {
+                ws.Cells[1, i].Value = header;
+                i++;
+            }
+            int rowIndex = 2;
+            foreach (FileInfo file in files)
+            {   try
+                {
+                    IDictionary<string, string> row = extractDataFromContactosFile(file.FullName);
+                    if (row == null)
+                    {
+                        File.Copy(file.FullName, Path.Combine(noProcesadorDir.FullName, file.Name));
+                        continue;
+                    }
+                    int colIndex = 1;
+                    foreach (string value in row.Values)
+                    {
+                        ws.Cells[rowIndex, colIndex].Value = value;
+                        colIndex++;
+                    }
+                    rowIndex++;
+                } catch (Exception error)
+                {
+                    File.Copy(file.FullName, Path.Combine(noProcesadorDir.FullName, file.Name));
+                }
+
+            }
+            string savePath = Path.Combine(folderTextBox.Text, "consolidad.xlsx");
+            p.SaveAs(new FileInfo(savePath));
+            const string Text1 = "Se ha creado el archivo consolidado.xslx en la carpeta selecionada, los archivos que no han podido procesarse estan en la carpeta \"no porcesados\"";
+            MessageBox.Show(Text1);
+            folderTextBox.Text = "";
+        }
+    }
+}
