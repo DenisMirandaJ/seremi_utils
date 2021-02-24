e2188d5a7a176212b87039d496953f488394e046
Denis <denis.mj@outlook.com>
2021-02-24 17:47:45 -0300

    AuditorCEL

---
+++ auditadorCarpetasCEL/auditadorCEL.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+def auditarCEL(planillas_dirname, respaldo_paciente_dirname):
+  files_in_planillas = []
+  for root, dirs, files in os.walk(planillas_dirname, topdown=False):
+    for file in files:
+      files_in_planillas.append(str(file))
+
+  # not_in_planillas = []
+  # for root, dirs, files in os.walk(respaldo_paciente_dirname, topdown=False):
+  #   if not os.path.splitext(files) not in ('xlsx', 'xls'):
+  #     os.remove()
+  #   if files not in files_in_planillas:
+  #     not_in_planillas.append([files, dirs])
+
+  open("a.txt", "w").writelines(files_in_planillas)
+
+auditarCEL('E:/nathalya', "")
